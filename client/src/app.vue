f62a214881692297d7426c4f4772f77c33405409
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-11-12 23:49:50 +0500

    обновлена функция вывода голоса на экран

--- client/src/app.vue
+++ client/src/app.vue
@@ -1,31 +1,41 @@
 <script setup>
+  import { ref } from "vue";
   import axios from "axios";
   import Reactor from "./modules/reactor/mod.vue";
 
-  const data = () => ({
-    apiResponse: "",
-  });
+  const apiResponse = ref("");
 
   const methods = {
     async callPythonScript() {
-      // debugger;
       console.log("Клик по кнопке!");
       try {
         const response = await axios.get("/run-python-script");
-        this.apiResponse = response.data;
+        apiResponse.value = response.data;
       } catch (error) {
         console.error("Ошибка при вызове Python-скрипта:", error);
       }
     },
+  };
+
+  // Устанавливаем серверное соединение для обновлений
+  const eventSource = new EventSource("/recognition-updates");
+  eventSource.onmessage = (event) => {
+    apiResponse.value = event.data;
   };
 </script>
 
 <template>
   <div>
     <button @click="methods.callPythonScript">Вызвать Python Script</button>
-    <p>{{ apiResponse }}</p>
+    <p class="logger">{{ apiResponse }}</p>
   </div>
   <reactor />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .logger {
+    font-size: 22px;
+    text-align: center;
+    color: #ffffff !important;
+  }
+</style>

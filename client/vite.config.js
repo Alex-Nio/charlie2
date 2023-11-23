1d13be536ba64f25b2363c18d3730272d8513168
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-11-24 00:52:44 +0500

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  clearScreen: false,
+  clearScreen: true,
   server: {
     port: 8080,
     strictPort: true

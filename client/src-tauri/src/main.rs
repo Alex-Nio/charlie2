6b5154a86110c0adf1bc31a7881106d2b1d97be0
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-11-24 14:21:25 +0500

    add icons

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,5 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[macro_use]
 extern crate lazy_static; // better switch to once_cell ?

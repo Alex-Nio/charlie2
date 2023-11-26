b92ede9ad640cb8c8b6f11e1000a0829f4b9bf17
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-11-27 01:03:54 +0500

    -add terminate to TTS -add context to request

--- client/src-tauri/commands/charlie/ahk/open_commands.ahk
+++ client/src-tauri/commands/charlie/ahk/open_commands.ahk
@@ -1,1 +1,11 @@
-Run "C:\Users\volpe\Desktop\Charlie\charlie2\client\src-tauri\commands"
+; Получаем путь к папке, где находится исполняемый файл скрипта
+ScriptDir := A_ScriptDir
+
+; Указываем поддиректорию
+SubDir := "Subfolder"
+
+; Формируем полный путь к поддиректории
+FullPath := ScriptDir . "\" . SubDir . "\" . SubDir
+
+; Открываем папку в проводнике
+Run, explorer %FullPath%
---
+++ client/src-tauri/src/tauri_commands/tts.rs
@@ -0,0 +1,125 @@
+use std::process::{Command, Child};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+
+// Структура, чтобы хранить информацию о процессе TTS
+struct TTSProcess {
+    child: Option<Child>,
+    stop_requested: bool,
+}
+
+impl TTSProcess {
+    fn new() -> Self {
+        TTSProcess {
+            child: None,
+            stop_requested: false,
+        }
+    }
+
+    fn stop_tts_static(process: &mut TTSProcess) {
+        // Прерываем процесс TTS, если он запущен
+        if let Some(mut child) = process.child.take() {
+            // Посылаем сигнал завершения процесса
+            let _ = child.kill();
+            // Ждем завершения процесса
+            let _ = child.wait();
+        }
+
+        // Добавьте небольшую задержку перед возвращением из функции
+        // Это может дать процессу TTS немного времени на полное завершение
+        thread::sleep(Duration::from_millis(200));
+    }
+
+    fn start_tts(&mut self, text: &str) -> Result<(), String> {
+        // Проверяем, не запущен ли уже процесс TTS
+        if self.child.is_some() {
+            // Ожидаем завершения предыдущего процесса
+            if let Some(mut child) = self.child.take() {
+                let _ = child.wait();
+            }
+        }
+
+        // Проверяем, был ли запрошен останов TTS
+        if self.stop_requested {
+            self.stop_requested = false; // Сбрасываем флаг
+            return Ok(());
+        }
+
+        // Создаем команду для запуска Python-скрипта
+        let mut command = if cfg!(target_os = "windows") {
+            Command::new("python")
+        } else {
+            Command::new("python3")
+        };
+
+        println!("Подготовка TTS...");
+
+        // Устанавливаем путь к скрипту и аргументы
+        let command = command
+            .arg("src/tts/tts_module.py")
+            .arg(text)
+            .spawn();
+
+        // Сохраняем Child-процесс для последующего управления им
+        match command {
+            Ok(child) => {
+                self.child = Some(child);
+                Ok(())
+            }
+            Err(err) => Err(format!("Error running Python script: {}", err)),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn stop_tts(&mut self) {
+        self.stop_requested = true;
+        // Прерываем процесс TTS, если он запущен
+        if let Some(mut child) = self.child.take() {
+            // Посылаем сигнал завершения процесса
+            let _ = child.kill();
+            // Ждем завершения процесса
+            let _ = child.wait();
+        }
+    }
+}
+
+// Общая переменная для использования внутри асинхронных функций
+// Мьютекс (Mutex) используется для обеспечения безопасности доступа к структуре TTSProcess
+lazy_static::lazy_static! {
+    static ref TTS_PROCESS: Arc<Mutex<TTSProcess>> = Arc::new(Mutex::new(TTSProcess::new()));
+}
+
+#[tauri::command]
+pub async fn speak_text(text: String) -> Result<(), String> {
+    // Создаем копию текста
+    let text_copy = text.clone();
+
+    // Получаем доступ к общей переменной
+    let tts_process = TTS_PROCESS.clone();
+
+    // Запускаем TTS в отдельном потоке
+    thread::spawn(move || {
+        let mut tts_process = tts_process.lock().unwrap();
+        if let Err(err) = tts_process.start_tts(&text_copy) {
+            eprintln!("Failed to start TTS: {}", err);
+        }
+    });
+
+    Ok(())
+}
+
+#[tauri::command]
+pub fn stop_tts() -> Result<(), String> {
+    // Получаем доступ к общей переменной
+    let tts_process = TTS_PROCESS.clone();
+
+    // Останавливаем TTS в отдельном потоке
+    thread::spawn(move || {
+        let mut tts_process = tts_process.lock().unwrap();
+        TTSProcess::stop_tts_static(&mut tts_process);
+    });
+
+    Ok(())
+}

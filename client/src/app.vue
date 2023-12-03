2b63a828b728910b4dc4a08a9db2a07b1fd19bc4
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-12-04 00:02:34 +0500

    update

--- client/src/app.vue
+++ client/src/app.vue
@@ -1,22 +1,18 @@
 <script setup>
-  import Reactor from "./modules/reactor/mod.vue";
-  import { ref, onMounted } from "vue";
+  // imports
+  import { ref } from "vue";
   import { invoke } from "@tauri-apps/api/tauri";
   import { listen } from "@tauri-apps/api/event";
 
+  // Components
+  import Titlebar from "./modules/titlebar/titlebar.vue";
+  import Details from "./modules/details/details.vue";
+  import Reactor from "./modules/reactor/reactor.vue";
+
+  // Consts
   const isListening = ref(true);
   const isReactorActive = ref(false);
-  const resourcesRamUsage = ref("-");
   const assistantVoiceVal = "jarvis-og";
-  const selectedMicrophone = ref(0);
-  const microphoneLabel = ref("");
-
-  // State
-  const nnDetails = ref({
-    sttEngine: "Vosk",
-    ttsEngine: "Silero",
-    gptEngine: "gpt-3.5-turbo-1106",
-  });
 
   // Functions
   const startListening = async () => {
@@ -66,103 +62,22 @@
     await listen("audio-play", playAudio);
   };
 
-  const updateResourcesRamUsage = async () => {
-    try {
-      resourcesRamUsage.value = Number(await invoke("get_current_ram_usage")).toFixed(2);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const setupMicrophone = async () => {
-    const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
-    try {
-      selectedMicrophone.value = +Number(await invoke("db_read", { key: "selected_microphone" }));
-      microphoneLabel.value = await invoke("pv_get_audio_device_name", {
-        idx: selectedMicrophone.value,
-      });
-
-      nnDetails.value.wwEngine = capitalizeFirstLetter(await invoke("db_read", { key: "selected_wake_word_engine" }));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // Lifecycle hook
-  onMounted(async () => {
-    setInterval(updateResourcesRamUsage, 1000);
-    await setupMicrophone();
-  });
-
   // Initial setup
   startListening();
   listenToAudioPlay();
 </script>
 
 <template>
-  <Reactor :isReactorActive="isReactorActive" />
-  <div>
-    <!-- Микрофон -->
-    <div class="online">
-      <div class="pulse">
-        <div class="wave"></div>
-      </div>
-      <div class="info">
-        <span class="num">Микрофон:</span>
-        <small title="{{ microphoneLabel }}">{{ microphoneLabel }}</small>
-      </div>
-    </div>
+  <Titlebar />
 
-    <!-- Нейросети -->
-    <div class="files">
-      <div class="pulse">
-        <div class="wave"></div>
-      </div>
-      <div class="info">
-        <span class="num">Нейросети:</span>
-        <small>{{ nnDetails.sttEngine }} + {{ nnDetails.ttsEngine }} + {{ nnDetails.gptEngine }}</small>
-      </div>
-    </div>
-
-    <!-- Ресурсы -->
-    <div class="info">
-      <span class="num">Ресурсы:</span>
-      <small> RAM {{ resourcesRamUsage }}mb </small>
-    </div>
+  <div class="full-wrapper">
+    <Reactor :isReactorActive="isReactorActive" />
+    <Details />
   </div>
 </template>
 
 <style lang="scss">
-body {
-  font-family: 'Roboto', sans-serif;
-  overflow-x: hidden;
-  backface-visibility: hidden;
-  -webkit-backface-visibility: hidden;
-}
-
-.logger {
-  font-size: 22px;
-  text-align: center;
-  color: #ffffff !important;
-}
-
-.info {
-  display: flex;
-  color: rgb(255, 255, 255);
-  gap: 8px;
-  z-index: 10;
-
-  & small {
-    font-size: 12px;
-  }
-}
-
-.num {
-  font-size: 12px;
-  font-weight: bold;
-  color: #00bf08;
+#app {
+  margin-top: 48px;
 }
 </style>
